396f7139e9a24e79fd6a2ba3965f57ef35a37929
Daniel Isaac <daniel.isaac@citrix.com>
2020-02-22 23:36:19 +0530

    basic product_catalogue & accounts page

--- sales_portal/src/app.py
+++ sales_portal/src/app.py
@@ -14,11 +14,51 @@ def sales_portal():
 def leads_page():
 	return 'Welcome to our Sales Portal Main Page <br/> These are all our leads  <br/> &nbsp;&nbsp; Leads  <br/> &nbsp;&nbsp; Mr. X  <br/> &nbsp;&nbsp; Mr. Y'
 
+@app.route('/opportunities_page')
+def opportunities_page():
+	return 'Welcome to our Sales Portal Main Page <br/> These are all our opportunities  <br/> &nbsp;&nbsp; Software Engineer 2  <br/> &nbsp;&nbsp; Principal SW Engineer <br/> &nbsp;&nbsp; PM Engineer'
+
+@app.route('/product_catalogue_page')
+def product_catalogue_page():
+	response = 'Welcome to the Main Product Catalogue Page. These are a list of all our items'
+	response += str(requests.get("http://catalogue-store/catalogue_store_page").content)
+	return response
+
+@app.route('/catalogue_store_page')
+def catalogue_store_page():
+	return 'Items <br/> Item 1 <br/> Item 2 <br/>'
+
+@app.route('/payables_page')
+def payables_page():
+	return 'Total Payables: $1023'
+
+@app.route('/receivables_page')
+def receivables_page():
+	return 'Total Receivables: $230'
+
+@app.route('/accounts_page')
+def accounts_page():
+	response = 'Welcome to the Main Accounts Page. These are a list of all your Receivables & Payables'
+	response += str(requests.get("http://payables/payables_page").content)
+	response += str(requests.get("http://receivables/receivables_page").content)
+	return response
+
 @app.route('/leads')
 def leads():
-	try:
-		r = requests.get("http://leads/leads_page")
-		return r.content
-	except:
-		return 'Unable to connect to Leads service'
+	response = requests.get("http://leads/leads_page")
+	return response.content
 
+@app.route('/opportunities')
+def opportunities():
+	response = requests.get("http://opportunities/opportunities_page")
+	return response.content
+
+@app.route('/product_catalogue')
+def product_catalogue():
+	response = requests.get("http://product-catalogue/product_catalogue_page")
+	return response.content
+
+@app.route('/accounts')
+def accounts():
+	response = requests.get("http://accounts/accounts_page")
+	return response.content
