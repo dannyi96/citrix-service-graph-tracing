d8de798fb4c5581cdde10b9458c4610670603a8b
Daniel Isaac <danielisaac@Daniels-MacBook-Pro.local>
2020-03-06 14:26:59 +0530

    k8 basic netflix app yaml changes

--- app/src/dockerhub_upload.sh
+++ app/src/dockerhub_upload.sh
@@ -1,3 +1,3 @@
 docker build -t sales-portal .
-docker tag sales-portal danny3112/sales-portal
-docker push danny3112/sales-portal
+docker tag sales-portal danny3112/netflix
+docker push danny3112/netflix
